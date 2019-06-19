6441c4bb74d0541d4862e5821f5cb56dbaa8237f
madhuprya <priyamadhu536@gmail.com>
2019-06-19 11:14:23 +0530

    checked item update working

--- trello.js
+++ trello.js
@@ -3,6 +3,7 @@ var key = "528ef6da60343fa8086bac0831fd43aa";
 var trelloId = "uG0ypKxz";
 
 var endpoints = `https://api.trello.com/1/boards/${trelloId}/lists?key=${key}&token=${token}`;
+var cardId;
 
 function getBoardList(bInd, cInd) {
     fetch(endpoints)
@@ -19,6 +20,7 @@ function getCardLists(data, c) {
 }
 
 function getCheckLists(data) {
+    cardId = data['id'];
     fetch(`https://api.trello.com/1/cards/${data['id']}/checklists?key=${key}&token=${token}`)
         .then(response => response.json())
         .then(data => data.map(list => list['checkItems']))
@@ -26,29 +28,84 @@ function getCheckLists(data) {
             item.map(i => accumulator.push(i));
             return accumulator;
         }, []))
-        .then(data => getItems(data));
+        .then(data => generateCheckitems(data));
 }
 
 
 
 /******************* DOM MANIPULATION ******************/
-function getItems(data) {
-    var items = data.map(data => data.name);
-    generateCheckitems(items);
-}
+
 
 getBoardList(2, 0);
 
 function generateCheckitems(items) {
+    console.log(items);
+    var val = 1;
     items.map(item => {
-        const c='\xf00d';
-        let i = `<li><input type="checkbox">${item}<i>${c}</i></li><hr>`;
+        let i = `<li><input class="check-items" id=${item.id} type="checkbox"><a>${item.name}</a><i type="button">&times;</i></li><hr>`;
+
         $("#check-lists").append(i).css({
             "list-style": "none",
             "background-color": "#d9eaad",
             "font-size": "20px",
             "padding": "100px 300px",
-            "margin":"100px 300px"
-        }).height("300px").width("500px");
+            "margin": "100px 300px",
+            "cursor": "pointer"
+        }).height("300px").width("300px");
+
+
+        fetch(`https://api.trello.com/1/cards/${cardId}/checkItem/${item.id}/`)
+            .then(response => response.json())
+            .then((Object) => {
+                (Object["state"] === "complete") ?
+                $(`#${item.id}`).prop("checked", true) &&
+                    $(`#${item.id}`).next().css({
+                        "text-decoration": "line-through",
+                        "color": "gray"
+                    }):
+                    $(`#${item.id}`).prop("checked", false) &&
+                    $(`#${item.id}`).next().css({
+                        "text-decoration": "none"
+                    })
+            });
     })
-}
+    $("#check-lists>li").css({
+        "display": "flex",
+        "flex-flow": "row nowrap",
+        "justify-content": "space-between",
+        "color": "red"
+    });
+
+
+
+    $("input").on("click", function () {
+                if ($(this).is(':checked')) {
+                    fetch(`https://api.trello.com/1/cards/${cardId}/checkItem/${this.id}?state=complete&key=${key}&token=${token}`, {
+                            method: 'PUT'
+                        })
+                        .then(data => data.json())
+                        .then((Object) => {
+                            Object["state"] = "complete"
+                        })
+                        .then(() => $(`#${this.id}`).next().css({
+                                    "text-decoration": "line-through",
+                                    "color": "gray"})
+                                    )
+                            }
+                            else {
+                                fetch(`https://api.trello.com/1/cards/${cardId}/checkItem/${this.id}?state=incomplete&key=${key}&token=${token}`, {
+                                        method: 'PUT'
+                                    })
+                                    .then(data => data.json())
+                                    .then((Object) => {
+                                        Object["state"] = "incomplete"
+                                    })
+                                    .then(() => $(`#${this.id}`).next().css({
+                                        "text-decoration": "none",
+                                        "color": "red"})
+                                        )
+                            }
+                        });
+
+
+            }
